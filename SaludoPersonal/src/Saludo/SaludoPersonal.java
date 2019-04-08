ea4ac8adc43124a08ac070490b6bcc1ebd9d738f
Mauro GC <mauriciogi@utp.edu.co>
2019-04-08 11:40:55 -0500

    Correción de error

---
+++ SaludoPersonal/src/Saludo/SaludoPersonal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Saludo;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SaludoPersonal {
+    public static void main(String args[]){
+        
+        System.out.println("Cesar, bienvenido al curso de Programación en Java!!!"); // imprime un saludo en 
+    }
+} en Java
