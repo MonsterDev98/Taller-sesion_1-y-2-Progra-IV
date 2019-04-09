aed76718af7eb4e09887e0c0aaac62149ab38b29
Mauro GC <mauriciogi@utp.edu.co>
2019-04-08 19:13:08 -0500

    Cadenas usando JOptionPane

---
+++ PalabrasJOP/src/cadenajop/CadenasJOP.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadenajop;
+import javax.swing.JOptionPane;
+
+public class CadenasJOP {
+    public static void main(String args[]){
+        
+        String palabra1, palabra2, palabra3;
+        
+        palabra1 = JOptionPane.showInputDialog("Ingrese la palabra1");
+        palabra2 = JOptionPane.showInputDialog("Ingrese la palabra2");
+        palabra3 = JOptionPane.showInputDialog("Ingrese la palabra3");
+        String mensaje = palabra1 + " " + " " + palabra2 + " " + palabra3;
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+}
